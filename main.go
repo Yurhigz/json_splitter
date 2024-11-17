82b082600ef9ed71165e8bde1c1c5df6f702f0be
Benoit.G <benoitgaugiran@gmail.com>
2024-11-17 22:52:46 +0100

    test

--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"json-splitter/utils"
+	"os"
 )
 
 func main() {
-	var userInput string
+	// var userInput string
 
-	fmt.Scan(&userInput)
+	// fmt.Scan(&userInput)
 
-	if utils.JsonChecking(userInput) != nil {
+	// if utils.JsonChecking(userInput) != nil {
+	// 	return
+	// }
+
+	// Open our jsonFile
+	jsonFile, err := os.Open("D:/Mes_documents/Programmation/json_splitter/test.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	r := bufio.NewReader(jsonFile)
+
+	// fmt.Println(r)
+
+	data := make([]byte, 1000)
+
+	_, err = r.Read(data)
+
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
+	fmt.Println(string(data))
 }
